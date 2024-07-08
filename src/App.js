a3404b0deecc644aef394a501ae192ce4c628764
Qilin Xie <146989043+QILINXIE02@users.noreply.github.com>
2024-07-08 13:30:10 -0700

    refactored App.js

--- src/App.js
+++ src/App.js
@@ -8,45 +8,28 @@ import About from "./components/About";
 import Home from "./components/Home";
 
 class App extends Component {
+  state = {
+    foo: "bar",
+    resumeData: {},
+    sharedData: {},
+  };
 
-  constructor(props) {
-    super();
-    this.state = {
-      foo: "bar",
-      resumeData: {},
-      sharedData: {},
-    };
+  componentDidMount() {
+    this.loadSharedData();
+    this.applyPickedLanguage(window.$primaryLanguage, window.$secondaryLanguageIconId);
   }
 
   applyPickedLanguage = (pickedLanguage, oppositeLangIconId) => {
     this.swapCurrentlyActiveLanguage(oppositeLangIconId);
     document.documentElement.lang = pickedLanguage;
-    var resumePath =
-      document.documentElement.lang === window.$primaryLanguage
-        ? `res_primaryLanguage.json`
-        : `res_secondaryLanguage.json`;
+    const resumePath = pickedLanguage === window.$primaryLanguage ? 'res_primaryLanguage.json' : 'res_secondaryLanguage.json';
     this.loadResumeFromPath(resumePath);
   }
 
   swapCurrentlyActiveLanguage = (oppositeLangIconId) => {
-    var pickedLangIconId =
-      oppositeLangIconId === window.$primaryLanguageIconId
-        ? window.$secondaryLanguageIconId
-        : window.$primaryLanguageIconId;
-    document
-      .getElementById(oppositeLangIconId)
-      .removeAttribute("filter", "brightness(40%)");
-    document
-      .getElementById(pickedLangIconId)
-      .setAttribute("filter", "brightness(40%)");
-  }
-
-  componentDidMount = () => {
-    this.loadSharedData();
-    this.applyPickedLanguage(
-      window.$primaryLanguage,
-      window.$secondaryLanguageIconId
-    );
+    const pickedLangIconId = oppositeLangIconId === window.$primaryLanguageIconId ? window.$secondaryLanguageIconId : window.$primaryLanguageIconId;
+    document.getElementById(oppositeLangIconId).removeAttribute("filter");
+    document.getElementById(pickedLangIconId).setAttribute("filter", "brightness(40%)");
   }
 
   loadResumeFromPath = (path) => {
@@ -54,12 +37,8 @@ class App extends Component {
       url: path,
       dataType: "json",
       cache: false,
-      success: function (data) {
-        this.setState({ resumeData: data });
-      }.bind(this),
-      error: function (xhr, status, err) {
-        alert(err);
-      },
+      success: (data) => this.setState({ resumeData: data }),
+      error: (xhr, status, err) => alert(err),
     });
   }
 
@@ -68,39 +47,21 @@ class App extends Component {
       url: `portfolio_shared_data.json`,
       dataType: "json",
       cache: false,
-      success: function (data) {
-        this.setState({ sharedData: data }, () => document.title = `${this.state.sharedData.basic_info.name}`);
-      }.bind(this),
-      error: function (xhr, status, err) {
-        alert(err);
-      },
+      success: (data) => this.setState({ sharedData: data }, () => document.title = data.basic_info.name),
+      error: (xhr, status, err) => alert(err),
     });
   }
 
   render() {
+    const { resumeData, sharedData } = this.state;
     return (
       <Router>
-        <Header sharedData={this.state.sharedData.basic_info} />
+        <Header sharedData={sharedData.basic_info} />
         <Routes>
-          <Route 
-            exact path="/" 
-            element={<Home
-              resumeData={this.state.resumeData}
-              sharedData={this.state.sharedData}
-            />}>
-          </Route>
-          <Route 
-            path="/about" 
-            element={<About
-              resumeBasicInfo={this.state.resumeData.basic_info}
-              sharedBasicInfo={this.state.sharedData.basic_info}
-            />}>
-          </Route>
+          <Route path="/" element={<Home resumeData={resumeData} sharedData={sharedData} />} />
+          <Route path="/about" element={<About resumeBasicInfo={resumeData.basic_info} sharedBasicInfo={sharedData.basic_info} />} />
         </Routes>
-        <Footer 
-          sharedBasicInfo={this.state.sharedData.basic_info}
-          applyPickedLanguage={this.applyPickedLanguage} 
-        />  
+        <Footer sharedBasicInfo={sharedData.basic_info} applyPickedLanguage={this.applyPickedLanguage} />
       </Router>
     );
   }
